fe9d27de0f653786939720dfb94fd28da1e4220f
Danilo <danilorivero95@gmail.com>
2021-12-08 19:35:48 +0000

    La password recuperada no se envia hasta que se cumplen sus condiciones

--- nuevaClave.php
+++ nuevaClave.php
@@ -9,16 +9,65 @@
 </head>
 <body>
     <div id="contenedor0">
-        <form action="crearNuevaClave.php" method="post">
+        <form action="crearNuevaClave.php" method="post" id="formularioRestablecerPassword">
             <div id="restablecerPassword3">
                 <h1>Restablecer Contraseña</h1>
                 <input type="text" name="idUsuario" value="<?php echo $_REQUEST['idUsuario']; ?>" hidden="true">
                 <input type="text" name="token" value="<?php echo $_REQUEST['token']; ?>" hidden="true">
-                <input type="password" placeholder="Nueva contraseña" name="password" class="rellenarRegistro">
-                <input type="password" placeholder="Repetir contraseña" class="rellenarRegistro">
-                <input type="submit" value="Modificar" class="botonRestablecer">
+                <input type="password" id="passwordRecuperar" placeholder="Nueva contraseña" name="password" class="rellenarRegistro" onkeyup="comprobarPassword(this.value);">
+                <span id="spanPasswordRecuperar" class="spanError">* La contraseña debe contener letras mayusculas, minuscula, numeros y 8 caracteres como minimo</span>
+                <input type="password" id="password2Recuperar" placeholder="Repetir contraseña" class="rellenarRegistro" onkeyup="comprobarPassword2(this.value)">
+                <span id="spanPassword2Recuperar" class="spanError">* La contraseña no coincide con la anterior</span>
+                <input type="button" value="Modificar" class="botonRestablecer" onclick="validarRegistro();">
             </div>
         </form>
     </div>
+    <script>
+        function comprobarPassword(password){
+            inputPassword = document.getElementById("passwordRecuperar");
+            mensajeError = document.getElementById("spanPasswordRecuperar");
+            regexp_password = /^(?=.*[a-zñ])(?=.*[A-ZÑ])(?=.*\d)[a-zñA-ZÑ\d]{8,}$/;
+            if(password.length<1){
+                inputPassword.style.border="1px solid grey";
+                mensajeError.style.display="none";
+            }
+            else if (!regexp_password.test(inputPassword.value)) {
+                inputPassword.style.border="2px solid red";
+                mensajeError.style.display="block";
+            }
+            else{
+                inputPassword.style.border="2px solid green";
+                mensajeError.style.display="none";
+            }
+        }
+        function comprobarPassword2(password2){
+            mensajeError = document.getElementById("spanPassword2Recuperar");
+            inputPassword = document.getElementById("passwordRecuperar").value;
+            inputPassword2 = document.getElementById("password2Recuperar");
+            if(password2 != inputPassword){
+                inputPassword2.style.border="2px solid red";
+                mensajeError.style.display="block";
+
+            }
+            else{
+                inputPassword2.style.border="2px solid green";
+                mensajeError.style.display="none";
+
+            }
+        }
+
+        function validarRegistro(){
+            var password = document.getElementById("passwordRecuperar");
+            var password2 = document.getElementById("password2Recuperar");
+            var filterPassword = /^(?=.*[a-zñ])(?=.*[A-ZÑ])(?=.*\d)[a-zñA-ZÑ\d]{8,}$/;
+
+            if (!filterPassword.test(password.value) || password.value!=password2.value) {
+                return;
+            }
+            else{
+                document.getElementById("formularioRestablecerPassword").submit();
+            }
+        }
+    </script>
 </body>
 </html>
